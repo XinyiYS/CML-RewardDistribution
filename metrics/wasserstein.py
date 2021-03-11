a33d172b5299a70fbdfc1efb68513c8db54eac61
Sebastian Tay <seb.tsh@gmail.com>
2021-03-11 15:24:11 +0800

    Add Wasserstein-2 estimate metric

---
+++ metrics/wasserstein.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def wasserstein_2(P, Q):
+    """
+
+    :param P: (m, d) matrix. Reference data
+    :param Q: (n, d) matrix. Distribution we are evaluating
+    :return: float
+    """
+
+    P_mean, P_cov = np.mean(P, axis=0), np.cov(P, rowvar=False)
+    Q_mean, Q_cov = np.mean(Q, axis=0), np.cov(Q, rowvar=False)
+
+    QP_diff = Q_mean - P_mean
+    return np.inner(QP_diff, QP_diff) + np.trace(Q_cov + P_cov - 2 * np.linalg.cholesky(Q_cov @ P_cov))
